6943cb5f9ce092f53409210dc4702d9d0b158b5c
Garen Sahagian <g.s.sahagian@gmail.com>
2020-05-13 12:29:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let foundText = document.querySelector("#text")
+    foundText.textContent = "This is really cool!"
+  });
